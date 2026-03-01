665c150f614802a1d1e6eecc192c491158160daa
Karan Goyal <karangoyal134@gmail.com>
2026-03-01 20:29:54 +0530

    myproject

--- Project 1/books.py
+++ Project 1/books.py
@@ -17,14 +17,14 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
-
+#path parameters
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
 
-
+#query parameters
 @app.get("/books/")
 async def read_category_by_query(category: str):
     books_to_return = []
